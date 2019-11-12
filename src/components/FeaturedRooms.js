149882729886688a4b369a00e01d5e2a282034be
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-11-12 18:46:48 +0100

    setup room and singleRoom components

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
 
 
 export default class FeaturedRooms extends React.Component {
@@ -9,14 +12,18 @@ export default class FeaturedRooms extends React.Component {
     
 
     render() {
-        const { featuredRooms: rooms } = this.context;
-        console.log(rooms);
-        
+        let { loading, featuredRooms: rooms } = this.context;
+            rooms = rooms.map(room => {
+                return <Room key={ room.id } room={ room } />
+            });
         
         return (
-            <div>
-                hello from featured rooms 
-            </div>
+            <section className="featured-rooms">
+                <Title title="featured rooms" />
+                <div className="featured-rooms-center">
+                    { loading ? <Loading /> : rooms }
+                </div>
+            </section>
         )
     }
 };
