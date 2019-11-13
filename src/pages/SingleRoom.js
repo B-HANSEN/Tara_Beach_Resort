2f8c7df4aa659acd72d40c0f98807a993aba1c22
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-11-13 17:52:05 +0100

    completed SingleRoom component with details, info and extras

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -40,9 +40,7 @@ export default class SingleRoom extends React.Component {
         
         // array destructuring:
         // use rest operator '...' instead of [ mainImg, img1, img2 ] ==> rest of the images in the array, apart from mainImg
-        const [ mainImg, ...defaultImg ] = images;
-        console.log(defaultImg);
-        
+        const [ mainImg, ...defaultImg ] = images;        
 
         return  (
             <>
@@ -64,8 +62,42 @@ export default class SingleRoom extends React.Component {
                             return <img key={ index } src={ item } alt={ name } />
                         })}
                     </div>
+
+                    <div className="single-room-info">
+                        <article className="desc">
+                            <h3>details</h3>
+                            <p>{ description }</p>
+                        </article>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>price: ${ price }</h6>
+                            <h6>size: ${ size } SQFT</h6>
+                            <h6>max capacity: {" "}
+                                { capacity > 1 
+                                ? `${ capacity } people`
+                                : `${ capacity } person `
+                                }
+                            </h6>
+                            <h6>
+                                { pets
+                                ? "pets allowed"
+                                : "no pets allowed"
+                                }
+                            </h6>
+                            <h6>{ breakfast && "free breakfast included"}</h6>
+                        </article>
+                    </div>
+                </section>
+
+                <section className="room-extras">
+                    <h6>extras</h6>
+                    <ul className="extras">
+                        { extras.map((item, index) => {
+                            return <li key={ index }>- { item }</li>
+                        })}
+                    </ul>
                 </section>
             </>
         )
-    }
-}
+    };
+};
