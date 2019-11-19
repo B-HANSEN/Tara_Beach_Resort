49140fbc5cba111f6fc2d3622c090ae583f3c616
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-11-19 19:52:07 +0100

    added room size, breakfast and pets filters

--- src/context.js
+++ src/context.js
@@ -95,6 +95,18 @@ filterRooms = () => {
 // filter by price
     tempRooms = tempRooms.filter(room => room.price <= price);
 
+// filter by size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+// filter by breakfast
+    if(breakfast) {
+        tempRooms = tempRooms.filter(room => room.breakfast === true);
+    };
+
+// filter by pets
+    if(pets) {
+        tempRooms = tempRooms.filter(room => room.pets === true);
+    };
 
 // change state    
     this.setState({ sortedRooms: tempRooms })
