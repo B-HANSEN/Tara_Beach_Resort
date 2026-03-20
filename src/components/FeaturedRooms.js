992a0a1a6f183ce1a67df9a8c9bd715038e36464
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2026-03-20 21:19:17 +0100

    refactor class to functional components

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { RoomContext } from '../context';
 import Loading from './Loading';
 import Room from './Room';
 import Title from './Title';
 
-export default class FeaturedRooms extends React.Component {
-    // code for a class-based component:
-    static contextType = RoomContext;
+export default function FeaturedRooms() {
+    const { loading, featuredRooms } = useContext(RoomContext);
 
-    render() {
-        let { loading, featuredRooms: rooms } = this.context;
-        rooms = rooms.map((room) => {
-            return <Room key={room.id} room={room} />;
-        });
-
-        return (
-            <section className="featured-rooms">
-                <Title title="featured rooms" />
-                <div className="featured-rooms-center">{loading ? <Loading /> : rooms}</div>
-            </section>
-        );
-    }
+    return (
+        <section className="featured-rooms">
+            <Title title="featured rooms" />
+            <div className="featured-rooms-center">
+                {loading
+                    ? <Loading />
+                    : featuredRooms.map((room) => <Room key={room.id} room={room} />)
+                }
+            </div>
+        </section>
+    );
 }
